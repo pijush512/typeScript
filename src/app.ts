fc96a82fa9e16c9a66b3ccfd5f5eaa176f53e485
pijush512 <psagorsarker512@gmail.com>
2026-07-02 20:25:20 +0600

    try to learn create two object using one common users type sciema in ts

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,38 @@ const name: string = "Mica";
 const age: number = 22;
 let isActive: boolean = true;
 
+console.log(name, age, isActive);
 
+// Array
+
+let digits: number [] = [1,2,3,4,5];
+const names: string [] = ["Mica", "Johan", "Luzi"];
+
+console.log(digits, names);
+
+// Object using type
+type users = {
+  id: number,
+  name: string,
+  class: string,
+  age: number,
+  isActive: boolean
+};
+
+let user1: users = {
+  id: 1,
+  name: "Mica",
+  class: "two",
+  age: 7,
+  isActive: true
+};
+
+let user2: users = {
+  id: 2,
+  name: "Luzi",
+  class: "five",
+  age: 10,
+  isActive: false
+};
+
+console.log(user1, user2)
